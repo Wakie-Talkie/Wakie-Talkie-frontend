dad907ab69f6b08edfbb0e3f9014232bed5e7b83
J2in <jiin62622@gmail.com>
2024-04-30 19:25:07 +0900

    [refactor] 일요일로 시작 데이터 수정

--- Wakie-Talkie/AlarmDataFetcher.swift
+++ Wakie-Talkie/AlarmDataFetcher.swift
@@ -13,13 +13,16 @@ class AlarmDataFetcher: ObservableObject {
     
     func fetchAlarms(){
         let fetchedAlarms = [
-            Alarm(id: "alarm1", userId: "eunhwa813", time: transformToDate(dateString: "9:05 PM"), language: "ENGLISH", repeatDays: [false, false, false, false, false, false, false], isOn: true),
-            Alarm(id: "alarm2", userId: "eunhwa813", time: transformToDate(dateString: "8:00 AM"), language: "KOREAN", repeatDays: [false, false, true, false, false, false, false], isOn: true),
-            Alarm(id: "alarm3", userId: "eunhwa813", time: transformToDate(dateString: "12:00 PM"), language: "JAPANESE", repeatDays: [true, false, false, false, true, false, false], isOn: false),
-            Alarm(id: "alarm4", userId: "eunhwa813", time: transformToDate(dateString: "7:00 PM"), language: "FRENCH", repeatDays: [false, true, false, false, false, false, false], isOn: true),
-            Alarm(id: "alarm5", userId: "eunhwa813", time: transformToDate(dateString: "1:20 PM"), language: "CHINESE", repeatDays: [false, false, false, false, false, true, false], isOn: false)
+            Alarm(id: "alarm1", userId: "eunhwa813", time: transformToDate(dateString: "1:48 PM"), language: "ENGLISH", repeatDays: [false, false, false, false, false, false, false], isOn: true),
+            Alarm(id: "alarm2", userId: "eunhwa813", time: transformToDate(dateString: "8:00 AM"), language: "KOREAN", repeatDays: [false, true, false, true, false, false, false], isOn: true),
+            Alarm(id: "alarm3", userId: "eunhwa813", time: transformToDate(dateString: "12:00 PM"), language: "JAPANESE", repeatDays: [false, true, false, false, false, true, false], isOn: false),
+            Alarm(id: "alarm4", userId: "eunhwa813", time: transformToDate(dateString: "7:00 PM"), language: "FRENCH", repeatDays: [false, false, true, false, false, false, false], isOn: true),
+            Alarm(id: "alarm5", userId: "eunhwa813", time: transformToDate(dateString: "1:20 PM"), language: "CHINESE", repeatDays: [false, false, false, false, false, false, true], isOn: false)
         ]
         self.alarms = fetchedAlarms
+
+        let a = AlarmManager()
+        a.scheduleNextAlarm(alarms: self.alarms ?? []) //TODO: 알람 생성, 수정시에도 이 함수 호출해야함.
     }
     
     func transformToDate(dateString: String) -> Date {
