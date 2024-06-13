def4bed20468656bc283c7d8d0976c3f5de7e04b
J2in <jiin62622@gmail.com>
2024-06-13 12:37:51 +0900

    [design] 프로필 화면 디자인 수정

--- Wakie-Talkie/View/MypageViews/ProfileView.swift
+++ Wakie-Talkie/View/MypageViews/ProfileView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ProfileView: View {
     @Environment(\.dismiss) var dismiss
     @State private var isPresentingProfileEdit = false
-    @State private var isPresentingRecodings = false
+    @State private var isPresentingRecordings = false
     @State private var isPresentingAiVoice = false
     @State private var isPresentingLogout = false
 //    @State private var recordDataFetcher = RecordDataFetcher()
@@ -54,25 +54,35 @@ struct ProfileView: View {
                         VStack(alignment: .leading) {
                             if let nickname = userData.user?.nickname {
                                 Text("\(nickname)")
-                                    .font(.headline)
+                                    .font(.system(size: 25))
                             }
                             if let wantLanguage =  userData.user?.wantLanguage {
                                 switch (wantLanguage) {
                                 case 1:
                                     Text("배우고 싶은 언어: English")
                                         .font(.subheadline)
+                                        .font(.system(size:16))
+                                        .fontWeight(.light)
                                 case 2:
                                     Text("배우고 싶은 언어: Korean")
                                         .font(.subheadline)
+                                        .font(.system(size:16))
+                                        .fontWeight(.light)
                                 case 3:
                                     Text("배우고 싶은 언어: Japanese")
                                         .font(.subheadline)
+                                        .font(.system(size:16))
+                                        .fontWeight(.light)
                                 case 4:
                                     Text("배우고 싶은 언어: Chinese")
                                         .font(.subheadline)
+                                        .font(.system(size:16))
+                                        .fontWeight(.light)
                                 default:
                                     Text("배우고 싶은 언어: 없음")
                                         .font(.subheadline)
+                                        .font(.system(size:16))
+                                        .fontWeight(.light)
                                 }
                                 
                             }
@@ -85,34 +95,56 @@ struct ProfileView: View {
                         isPresentingProfileEdit = true
                     }, color: Color("Black"), isActive: .constant(true))
                     
-                    List {
+                //    List {
+                    Button(action: {
+                        isPresentingRecordings = true
+                    }) {
                         Text("통화 기록 보기")
-                            .onTapGesture {
-                                isPresentingRecodings = true
-                            }
+                            .font(.system(size:20))
+                            .foregroundColor(.black)
+                            .fontWeight(.light)
+                    }.padding(EdgeInsets(top: 70, leading: 50, bottom: 27, trailing: 0))
+                        
+                    Button(action: {
+                        isPresentingAiVoice = true
+                    }) {
                         Text("AI 목소리 설정하기")
-                            .onTapGesture {
-                                isPresentingAiVoice = true
-                            }
+                            .font(.system(size:20))
+                            .foregroundColor(.black)
+                            .fontWeight(.light)
+                    }.padding(EdgeInsets(top: 27, leading: 50, bottom: 27, trailing: 0))
+                    Button(action: {
+                    
+                    }) {
                         Text("설정")
-                        Text("로그아웃")
-                            .onTapGesture {
-                                isPresentingLogout = true
-                            }.alert(isPresented: $isPresentingLogout) {
-                                Alert(
-                                    title : Text("Wakie-Talkie에서 로그아웃하시겠어요?"),
-                                    primaryButton : .default(Text("네"), action: {  }),
-                                    secondaryButton : .cancel(Text("아니오"))
-                                )
-                            }
-                    }
+                            .font(.system(size:20))
+                            .foregroundColor(.black)
+                            .fontWeight(.light)
+                    }.padding(EdgeInsets(top: 27, leading: 50, bottom: 27, trailing: 0))
+                    
+                
+                   
+                    Text("로그아웃")
+                        .font(.system(size:20))
+                        .fontWeight(.light)
+                        .onTapGesture {
+                            isPresentingLogout = true
+                        }.alert(isPresented: $isPresentingLogout) {
+                            Alert(
+                                title : Text("Wakie-Talkie에서 로그아웃하시겠어요?"),
+                                primaryButton : .default(Text("네"), action: {  }),
+                                secondaryButton : .cancel(Text("아니오"))
+                            )
+                        }.padding(EdgeInsets(top: 27, leading: 50, bottom: 27, trailing: 0))
+                    Spacer()
+                  //  }
                 }
             }
             .navigationBarBackButtonHidden(true)
             .navigationDestination(isPresented: $isPresentingProfileEdit){
                 ProfileEditView()
             }
-            .navigationDestination(isPresented: $isPresentingRecodings){
+            .navigationDestination(isPresented: $isPresentingRecordings){
                 CallRecodView()
             }
             .navigationDestination(isPresented: $isPresentingAiVoice){
